5bf946696e9d3f4f0de77145d27771b99c630f62
Maddy Cook <madison.cook@usu.edu>
2026-03-19 20:38:31 +0000

    date fixes

--- models/oliver/oliver_dim_date.sql
+++ models/oliver/oliver_dim_date.sql
@@ -1,8 +1,32 @@
 {{ config(materialized='view') }}
 
+with order_dates as (
+
+    select distinct
+        order_date as date_key
+    from {{ source('oliver', 'orders') }}
+
+),
+
+certification_dates as (
+
+    select distinct
+        parse_json(certification_json):certification_awarded_date::date as date_key
+    from {{ source('oliver', 'employee_certifications') }}
+
+),
+
+all_dates as (
+
+    select date_key from order_dates
+    union
+    select date_key from certification_dates
+
+)
+
 select distinct
-  order_date as date_key,
-  year(order_date) as year,
-  month(order_date) as month,
-  day(order_date) as day
-from {{ source('oliver', 'orders') }}
+    date_key,
+    year(date_key) as year,
+    month(date_key) as month,
+    day(date_key) as day
+from all_dates
